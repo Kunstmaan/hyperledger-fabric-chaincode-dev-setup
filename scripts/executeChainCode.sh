6b6724e105bc2ab98e83895654d9f4dd19178a8a
Jan Bevers <jan.bevers@kunstmaan.be>
2017-11-16 11:27:28 +0100

    Whenever chaincode changes run the upgrade command instead of an install

--- scripts/executeChainCode.sh
+++ scripts/executeChainCode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker exec -it -e VERSION=0 cli bash
+docker exec -it cli bash
 
-peer chaincode invoke -n mycc${VERSION} -c '{"function":"initLedger","Args":[]}' -C myc
-peer chaincode query -n mycc${VERSION} -c '{"function":"queryCar","Args":["CAR0"]}' -C myc
-peer chaincode invoke -n mycc${VERSION} -c '{"function":"createCar","Args":["CAR100","Ferrari","Scuderia","Red","Jan"]}' -C myc
+peer chaincode invoke -n mycc -c '{"function":"initLedger","Args":[]}' -C myc
+peer chaincode query -n mycc -c '{"function":"queryCar","Args":["CAR0"]}' -C myc
+peer chaincode invoke -n mycc -c '{"function":"createCar","Args":["CAR100","Ferrari","Scuderia","Red","Jan"]}' -C myc
--- scripts/upgradeChainCode.sh
+++ scripts/upgradeChainCode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker exec -i cli bash -c "peer chaincode install -p chaincode/${CHAINCODE_DIR} -n mycc -v ${VERSION} -l node"
-docker exec -i cli bash -c "peer chaincode instantiate -n mycc -v ${VERSION} -c '{\"Args\":[]}' -C myc -l node"
+docker exec -i cli bash -c "peer chaincode upgrade -n mycc -v ${VERSION} -c '{\"Args\":[]}' -C myc -l node"

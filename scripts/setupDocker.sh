ca8c4d2d203c93f9d0dcb428366f93ad54260952
Leurquin <guillaume.leurquin@kunstmaan.be>
2018-01-11 12:41:56 +0100

    update startup script

--- scripts/setupDocker.sh
+++ scripts/setupDocker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker rm -f couchdb cli chaincode peer orderer
-docker network rm chaincodedockerdevmode_default
+docker network rm hyperledgerNet
+docker network create --driver bridge --subnet=192.168.100.0/24 hyperledgerNet
 docker-compose -f ./chaincode-docker-devmode/docker-compose-simple.yaml up

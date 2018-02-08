c54894b81c19d7cd9859a069d3f213edf12b8ff7
Jan Bevers <jan.bevers@kunstmaan.be>
2018-02-08 16:44:03 +0100

    Don't pull docker images in case it's not installed

--- scripts/setupDockerImages.sh
+++ scripts/setupDockerImages.sh
@@ -3,22 +3,30 @@
 # Exit on first error
 set -e
 
-docker pull hyperledger/fabric-orderer:x86_64-1.1.0-preview
-docker tag hyperledger/fabric-orderer:x86_64-1.1.0-preview hyperledger/fabric-orderer:latest
+which docker
 
-docker pull hyperledger/fabric-couchdb:x86_64-1.1.0-preview
-docker tag hyperledger/fabric-couchdb:x86_64-1.1.0-preview hyperledger/fabric-couchdb:latest
+if [ $? -eq 0 ]
+then
+    docker pull hyperledger/fabric-orderer:x86_64-1.1.0-preview
+    docker tag hyperledger/fabric-orderer:x86_64-1.1.0-preview hyperledger/fabric-orderer:latest
 
-docker pull hyperledger/fabric-peer:x86_64-1.1.0-preview
-docker tag hyperledger/fabric-peer:x86_64-1.1.0-preview hyperledger/fabric-peer:latest
+    docker pull hyperledger/fabric-couchdb:x86_64-1.1.0-preview
+    docker tag hyperledger/fabric-couchdb:x86_64-1.1.0-preview hyperledger/fabric-couchdb:latest
 
-docker pull hyperledger/fabric-ca:x86_64-1.1.0-preview
-docker tag hyperledger/fabric-ca:x86_64-1.1.0-preview hyperledger/fabric-ca:latest
+    docker pull hyperledger/fabric-peer:x86_64-1.1.0-preview
+    docker tag hyperledger/fabric-peer:x86_64-1.1.0-preview hyperledger/fabric-peer:latest
 
-docker pull hyperledger/fabric-tools:x86_64-1.1.0-preview
-docker tag hyperledger/fabric-tools:x86_64-1.1.0-preview hyperledger/fabric-tools:latest
+    docker pull hyperledger/fabric-ca:x86_64-1.1.0-preview
+    docker tag hyperledger/fabric-ca:x86_64-1.1.0-preview hyperledger/fabric-ca:latest
 
-docker pull hyperledger/fabric-ccenv:x86_64-1.1.0-preview
-docker tag hyperledger/fabric-ccenv:x86_64-1.1.0-preview hyperledger/fabric-ccenv:latest
+    docker pull hyperledger/fabric-tools:x86_64-1.1.0-preview
+    docker tag hyperledger/fabric-tools:x86_64-1.1.0-preview hyperledger/fabric-tools:latest
 
-docker pull yeasy/hyperledger-fabric-couchdb
+    docker pull hyperledger/fabric-ccenv:x86_64-1.1.0-preview
+    docker tag hyperledger/fabric-ccenv:x86_64-1.1.0-preview hyperledger/fabric-ccenv:latest
+
+    docker pull yeasy/hyperledger-fabric-couchdb
+
+else
+    echo "docker not installed"
+fi

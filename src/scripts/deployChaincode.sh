eae733f2fad420ee5130b617819b874a7ff7f234
Jan Bevers <jan.bevers@kunstmaan.be>
2018-05-11 13:44:26 +0200

    Added environment variable to indicate the chaincode is deployed in dev mode

--- src/scripts/deployChaincode.sh
+++ src/scripts/deployChaincode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -i chaincode bash -c "cd ${CHAINCODE_NAME}; CORE_CHAINCODE_ID_NAME=${CHAINCODE_NAME}:${VERSION} npm start -- --peer.address=peer:7052"
+docker exec -i chaincode bash -c "cd ${CHAINCODE_NAME};  IS_DEV_ENVIRONMENT=true CORE_CHAINCODE_ID_NAME=${CHAINCODE_NAME}:${VERSION} npm start -- --peer.address=peer:7052"

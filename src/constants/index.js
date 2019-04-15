16e7fc59a757fa8aa85138fc02e45d41b4aa2bef
Jan Bevers <jan.bevers@kunstmaan.be>
2019-04-15 17:34:48 +0200

    Add option to set copyGlobPattern from package.json

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,11 +25,13 @@ module.exports = {
         'test': 'npm run lint && jest test/',
         'lint': 'eslint .'
     },
+    CONFIG_COPY_GLOB_PATTERN_KEY: 'copyGlobPattern',
 
     DEFAULT_CHANNEL: 'defaultchannel',
     DEFAULT_PEER_NAME: 'peer',
     DEFAULT_CHAINCODE_DESTINATION_PATH: path.resolve(__dirname, '../../dev-network/generated/devmode/chaincode'),
     DEFAULT_DOCKER_FILE: path.resolve(__dirname, '../../dev-network/generated/devmode/docker-compose-simple.yaml'),
+    DEFAULT_COPY_GLOB_PATTERN: '**/!(package-lock.json)',
     SCRIPTS_PATH: path.resolve(__dirname, '../scripts'),
     DOCKER_SETUP_FINISHED_REGEX: /^.*?orderer.*?\[orderer\/common\/deliver\]\s+deliverBlocks\s+->\s+DEBU\s[0-9].*?\[channel:\s+.*?\]\s+Received\s+seekInfo\s+\(.*?\)\s+start:.*?$/gmi,
     DEPLOY_FINISHED_REGEX: /^.*?Successfully\s+established\s+communication\s+with\s+peer\s+node\.\s+State\s+transferred\s+to\s+"ready".*?$/gmi
